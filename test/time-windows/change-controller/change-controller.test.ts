371ce9e50c834bf2637567a59c0019e135306915
cdklabs-automation <90142015+cdklabs-automation@users.noreply.github.com>
2023-03-22 05:36:33 -0700

    fix: upgrade lambda runtime to nodejs16.x (#124)
    * chore(deps): upgrade dependencies
    
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdklabs/cdk-codepipeline-extensions/actions/runs/4489037972
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * chore: upgrade lambda runtime to node16.x
    
    * fixup
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Momo Kornher <kornherm@amazon.co.uk>

--- test/time-windows/change-controller/change-controller.test.ts
+++ test/time-windows/change-controller/change-controller.test.ts
@@ -80,7 +80,7 @@ describe('Change Controller Tests', () => {
       Description:
         'src/time-windows/change-controller/change-controller.lambda.ts',
       Handler: 'index.handler',
-      Runtime: 'nodejs14.x',
+      Runtime: 'nodejs16.x',
     });
 
     // Only One
@@ -90,7 +90,7 @@ describe('Change Controller Tests', () => {
         Description:
           'src/time-windows/change-controller/change-controller.lambda.ts',
         Handler: 'index.handler',
-        Runtime: 'nodejs14.x',
+        Runtime: 'nodejs16.x',
       },
       1
     );
